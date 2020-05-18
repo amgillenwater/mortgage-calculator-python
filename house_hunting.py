bf574a99c7c295687895bc13b479c7b1ff495d0b
amgillenwater <amgillenwater@gmail.com>
2020-05-18 14:05:39 -0400

    the function is not exactly calculating correctly, but pretty sure it's a logic error with my equation in the while statement

--- house_hunting.py
+++ house_hunting.py
@@ -8,18 +8,21 @@ portion_down_payment = 0.25 * total_cost
 #current savings has to retain it's former value plus add the interest gained from that value AND add the new savings from the % of paycheck
 monthly_salary = annual_salary / 12
 monthly_savings = monthly_salary * portion_saved
-current_savings = 1666.666666666667
+current_savings = 0
 interest_earned = current_savings * r/12
-current_savings = current_savings + interest_earned + monthly_savings
 total_month_savings = monthly_savings + interest_earned
 #add together the amount saved and the amount earned from interest for monthly savings total
 
-print(current_savings)
+#print(current_savings)
 
 #the ultimate goal is to figure out how many months have to pass before current_savings = portion_down_payment
 months = 0
-current_savings = 0
 
-#while current_savings < portion_down_payment:
-   # months += 1
+
+while current_savings < portion_down_payment:
+    months += 1
+    current_savings = current_savings + interest_earned
+    current_savings += monthly_savings
+
+print(months)
 
