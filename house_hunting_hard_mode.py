f495a887ccc17d61ac78e9363eec40b5e1574779
amgillenwater <amgillenwater@gmail.com>
2020-05-18 20:13:51 -0400

    started hard mode in separate file to confuse myself less

---
+++ house_hunting_hard_mode.py
@@ -0,0 +1,26 @@
+total_cost = float(input("What is the cost of your dream home?"))
+annual_salary = float(input("What is your annual salary?"))
+portion_saved = float(input("What percent of your paycheck would you like to save, as a decimal?"))
+expected_rate = float(input("Enter the expected annual rate of return:") or "0.04")
+down_payment_percent = float(input("Enter the percent of your home's cost to save as a down payment:") or 0.25)
+
+#variables for calculating savings from salary
+r = expected_rate / 12
+portion_down_payment = down_payment_percent * total_cost
+#current savings has to retain it's former value plus add the interest gained from that value AND add the new savings from the % of paycheck
+monthly_salary = annual_salary / 12
+monthly_savings = monthly_salary * portion_saved
+current_savings = 0
+#add together the amount saved and the amount earned from interest for monthly savings total
+
+#print(current_savings)
+
+#the ultimate goal is to figure out how many months have to pass before current_savings = portion_down_payment
+months = 0
+
+
+while current_savings < portion_down_payment:
+    months += 1
+    current_savings = current_savings + (current_savings * r) + monthly_savings
+
+print("Number of months: " + str(months))
